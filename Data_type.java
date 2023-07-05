9712fb0fd2934a921f7ca500dee906ea764ec82d
Aditya7verma <129338439+Aditya7verma@users.noreply.github.com>
2023-07-05 19:46:54 +0530

    Add files via upload

---
+++ Data_type.java
@@ -0,0 +1,18 @@
+package Lac5;
+
+public class Data_type {
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+
+        float f;
+        double d;
+
+        char c;
+        boolean bo;
+
+
+    }
+}
